9d860ff2a7566a4d51ee23ce901985813b1c942a
Andrey Lisitsa <75456617+And-Li@users.noreply.github.com>
2022-01-27 18:40:21 +0800

    Added Dropout layers
    accuracy is 0.44%!!!

--- fashion_1.py
+++ fashion_1.py
@@ -17,8 +17,8 @@ print(x_test.shape, y_test.shape)
 
 x_train = x_train.reshape(x_train.shape[0], -1)
 x_test_reshaped = x_test.reshape(x_test.shape[0], -1)
-print(f'Форма обучающих данных: {x_train.shape} -> {x_train.shape}')
-print(f'Форма тестовых данных: {x_test.shape} -> {x_test.shape}')
+# print(f'Форма обучающих данных: {x_train.shape} -> {x_train.shape}')
+# print(f'Форма тестовых данных: {x_test.shape} -> {x_test.shape}')
 
 x_train = x_train.astype('float32')/255
 x_test_reshaped = x_test_reshaped.astype('float32')/255
@@ -26,24 +26,30 @@ x_test_reshaped = x_test_reshaped.astype('float32')/255
 CLASS_COUNT = 10
 y_train = utils.to_categorical(y_train, CLASS_COUNT)
 y_test = utils.to_categorical(y_test, CLASS_COUNT)
-print(y_train.shape)
+'''print(y_train.shape)
 print(y_train[0])
-print(x_train, y_train.shape)
+print(x_train, y_train.shape)'''
 
-# создаём модель из 3-х слоёв: 
+# создаём модель из 3-х слоёв:
 model = Sequential()
-model.add(Dense(800, input_dim=784, activation='relu'))
+model.add(Dropout(0.3, input_shape=(x_train.shape[1],)))
+model.add(Dense(800, activation='relu'))
+model.add(Dropout(0.3))
 model.add(Dense(400, activation='relu'))
+model.add(Dropout(0.3))
 model.add(Dense(CLASS_COUNT, activation='relu'))
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 print(model.summary())
-model.fit(x_train, y_train, batch_size=128, epochs=15, verbose=1)
+model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=1)
 
-n_rec = 389
+'''n_rec = 389
 plt.imshow(x_test[n_rec], cmap='gray')
-plt.show()
-
+plt.show()'''
+print()
+scores = model.evaluate(x_train, y_train, verbose=1)
+print('Total accuracy is: ', scores[1])
+print('Loss value is: ', scores[0])
 
